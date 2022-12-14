62973d01d113efeccddb43d76d3f05d891915219
Lucas Echeveste <echevestevillavicenciolucas@gmail.com>
2022-12-14 16:04:55 -0300

    Update: error en boton agregar copias arreglado Fecha: 14/12/2022 16:04

--- TP_Final/UI/EditCopies.cs
+++ TP_Final/UI/EditCopies.cs
@@ -64,9 +64,15 @@ namespace TP_Final.UI
         private void IconPictureBox1_Click_1(object sender, EventArgs e)
         {
             string amount = Microsoft.VisualBasic.Interaction.InputBox("Ingrese cantidad de copias: ", "Cantidad de copias");
+            if (amount == "")
+            {
+                amount = "0";
+            }
             int amount2 = Int32.Parse(amount);
             LibraryManager.AddCopy(iBook.ISBN, amount2);
             this.AssignElements();
+
+
         }
 
         private void IconPictureBox2_Click(object sender, EventArgs e)
